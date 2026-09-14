b9e0a0adc00bfd986948f2f3e7271d7db3041f16
jemny2121217-rgb <jemny2121217@gmail.com>
2026-09-14 09:25:14 +0545

    Update supabase.js

--- rydo-v1/js/supabase.js
+++ rydo-v1/js/supabase.js
@@ -1,1 +1,18 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
+const SUPABASE_URL = "https://pdfgqlvovmyzhdnumypq.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_cZnuDJwbDbtF2dDJ_M6rCg_-e-nWdMX";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
+);
